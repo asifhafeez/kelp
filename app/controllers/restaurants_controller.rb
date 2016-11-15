e2092f06403a168d8ea2b9c55ef9a3fb757efc97
Asif Hafeez <asif.hafeez@hotmail.co.uk>
2016-11-15 16:40:25 +0000

    closes this issue

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,15 +5,40 @@ class RestaurantsController < ApplicationController
 	end
 
 	def new
+		@restaurant = Restaurant.new
 	end
 
 	def create
-		restaurant = Restaurant.new(name: params[:name], description: params[:description])
-		flash[:success] = "Restaurant was successfully added" if restaurant.save
-		redirect_to '#index'
+		@restaurant = Restaurant.new(restaurant_params)
+		if @restaurant.save
+			flash[:success] = "Restaurant was successfully added" 
+			redirect_to '#index'
+		else
+			render 'new'
+		end
 	end
 
 	def show
 		@restaurant = Restaurant.find(params[:id])
 	end
+
+	def edit
+		@restaurant = Restaurant.find(params[:id])
+	end
+
+	def update
+		@restaurant = Restaurant.find(params[:id])
+		if @restaurant.update(restaurant_params)
+			flash[:success] = "Restaurant was successfully updated" 
+			redirect_to @restaurant
+		else
+			render 'edit'
+		end
+	end
+
+	private
+
+		def restaurant_params
+			params.require(:restaurant).permit(:name, :description)
+		end
 end
--- spec/features/add_restaurants_spec.rb
+++ spec/features/add_restaurants_spec.rb
@@ -2,9 +2,9 @@ feature 'Add restaurant' do
   scenario 'can add a restaurant' do
     visit '/'
     click_link 'Add restaurant'
-    fill_in :name, with: 'Hungry Donkey'
-    fill_in :description, with: 'The best greek restaurant in town'
-    expect{click_button 'Add'}.to change{Restaurant.count}.by(1)
+    fill_in :restaurant_name, with: 'Hungry Donkey'
+    fill_in :restaurant_description, with: 'The best greek restaurant in town'
+    expect{click_button 'Submit'}.to change{Restaurant.count}.by(1)
     expect(page).to have_content 'Hungry Donkey'
     expect(page).to have_content 'The best greek restaurant in town'
     expect(page).to have_content 'Restaurant was successfully added'
