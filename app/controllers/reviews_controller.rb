f1915cbecfc860e3a2c168b8ec10cf9e84fb01c7
Elizabeth Wicks <elizabeth.wicks@hotmail.com>
2016-11-15 11:45:08 +0000

    feature test for adding review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,4 +4,22 @@ class ReviewsController < ApplicationController
 
   end
 
+  def create
+    @review = Review.new(review_params)
+
+    @review.save
+    redirect_to @review
+  end
+
+  def show
+    @review = Review.find(params[:id])
+  end
+
+
+private
+
+  def review_params
+    params.require(:review).permit(:rating, :comment)
+  end
+
 end
