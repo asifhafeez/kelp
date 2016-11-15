c6c3fd36e0cc5b45e9ae8b91e9e192a0368309b1
Malinna Leach <malinnab@yahoo.com>
2016-11-15 10:39:14 +0000

    initial review setup

---
+++ app/controllers/review_controller.rb
@@ -0,0 +1,8 @@
+class ReviewController < ApplicationController
+
+  def new
+
+  end
+
+
+end
---
+++ spec/review/review_spec.rb
@@ -0,0 +1,8 @@
+describe Review do
+
+  it 'should save a new review in the database' do
+    
+  end
+
+
+end
