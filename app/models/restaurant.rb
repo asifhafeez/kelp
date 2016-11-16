fc572100d7f4791d95c4afcb03ab3fab4f98d3b3
Malinna Leach <malinnab@yahoo.com>
2016-11-16 11:34:01 +0000

    average rating working

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,12 @@ class Restaurant < ActiveRecord::Base
 
   has_many :reviews
 
+  def average_rating
+    unless self.reviews
+      self.reviews.average(:rating).round
+    else
+      "No ratings yet"
+    end
+  end
+
 end
