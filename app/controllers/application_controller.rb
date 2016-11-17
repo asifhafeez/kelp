21cce628a1a206eeb727e2f945fc1e0338094b7c
Malinna Leach <malinnab@yahoo.com>
2016-11-17 16:47:47 +0000

    users can delete own reviews

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,4 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  private
-
-  # Overwriting the sign_out redirect path method
-  def after_sign_out_path_for(resource_or_scope)
-    root_path
-  end
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  
+
     let(:user) { User.create email: 'tansaku@gmail.com', password: '12345678', password_confirmation: '12345678' }
-  
-  	subject(:review) {described_class.new(rating: "3", comment: "DONT ORDER HERE. Eat cereal you already have. See that canned soup in the pantry? Cook it. F$!@ this place! Never again!", 
-                      user_id: user.id)}
+    let(:user2) { User.create email: 'tansakuuu@gmail.com', password: '12345678', password_confirmation: '12345678' }
+    let(:restaurant) {Restaurant.create(name: "Tayyabs", description: "Great Indian Restaurant",
+  		user_id: user.id)}
+  	subject(:review) {described_class.create(rating: "3", comment: "DONT ORDER HERE. Eat cereal you already have. See that canned soup in the pantry? Cook it. F$!@ this place! Never again!",
+                      user_id: user2.id, restaurant_id: restaurant.id)}
 
   	it "should set the rating" do
   		expect(review.rating).to eq(3)
@@ -25,12 +27,17 @@ RSpec.describe Review, type: :model do
       expect {review2.save}.to_not change{Review.count}
     end
 
-    it "should have a user", focus: :true do
-      expect(review.user).to eq user
+    it "should have a user" do
+      expect(review.user).to eq user2
+    end
+
+    it "should have a restaurant" do
+      expect(review.restaurant).to eq restaurant
     end
 
     it "should have a rating less than or equal to 5" do
       review2 = Review.new(rating: "6")
       expect {review2.save}.to_not change{Review.count}
     end
+
   end
