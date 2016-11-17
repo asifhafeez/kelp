c5760768fe668e2b0e6ea5dc55e4aae0a0f66cf4
Malinna Leach <malinnab@yahoo.com>
2016-11-17 12:19:33 +0000

    displaying email is done

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,4 +5,10 @@ class Review < ActiveRecord::Base
   validates :user, presence: true
   validates :rating, presence: true, inclusion: 1..5
   validates :comment, length: { maximum: 1500 }
+
+  def get_user_email
+    user = User.find(user_id)
+    user.email
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
       resources :reviews
   end
 
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
